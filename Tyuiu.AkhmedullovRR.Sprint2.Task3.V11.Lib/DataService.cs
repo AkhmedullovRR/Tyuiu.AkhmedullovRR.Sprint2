b140e633e9e2318e8d2d0f7f65c96e7f0402f041
Равиль <fullbotrr@mail.ru>
2024-10-15 22:48:09 +0300

    hbbhj

--- Tyuiu.AkhmedullovRR.Sprint2.Task3.V11.Lib/DataService.cs
+++ Tyuiu.AkhmedullovRR.Sprint2.Task3.V11.Lib/DataService.cs
@@ -13,13 +13,13 @@ namespace Tyuiu.AkhmedullovRR.Sprint2.Task3.V11.Lib
         {
             double y = 0;
             if (x > 0)
-                y = x + Math.Pow((x - 15) / (2 - 19), x);
+                y = x + Math.Pow((x - 15) / (x - 19), x);
             if (x == 0)
                 y = (x * x - Math.Cos(x * x) + 10) / (x * x - Math.Sin(x * x) + 12);
             if (x > -15 && x < 0)
-                y = Math.Pow(1 + 1 / (x * x), x);
+                y = Math.Pow(1 + (1 / (x * x)), x);
             if (x < -15)
-                y = x + 10 * x - 1 / x;
+                y = x + 10 * x - 1 / x; 
 
             return y;
         }
